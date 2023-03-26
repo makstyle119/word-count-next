b4954e7fd51bf7089ceed1dc856012009fa82727
makstyle119 <moizprince786@gmail.com>
2023-03-26 19:42:58 +0500

    handle build failed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { SEOHeader } from './SEOHeader';
+import SEOHeader from './SEOHeader';
 
 export default function Home() {
 	const getYear: number = new Date().getFullYear();
