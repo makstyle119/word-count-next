4964a66c097afbb5b6ea2bb15604790b7aef9121
makstyle119 <moizprince786@gmail.com>
2023-03-26 19:22:53 +0500

    basic level app and seo kind stuff

---
+++ src/pages/SEOHeader.tsx
@@ -0,0 +1,51 @@
+import Head from 'next/head';
+
+export const SEOHeader = () => {
+	return (
+		<Head>
+			<title>Word Count App || MAK Development House || MAKSTYLE119</title>
+			<meta
+				name='viewport'
+				content='width=device-width, initial-scale=1.0'
+			/>
+			<title>Word Count App || MAK Development House || MAKSTYLE119</title>
+			<meta
+				name='description'
+				content="Use our word count app to easily count the number of words in your text. It's fast, free, and easy to use."
+			/>
+			<meta
+				name='keywords'
+				content='Word Count, app, js, makstyle119, Character count'
+			/>
+			<meta
+				name='author'
+				content='MAKSTYLE119'
+			/>
+			<meta
+				name='copyright'
+				content='MAKSTYLE119'
+			/>
+			<meta
+				name='page-topic'
+				content='Word count App'
+			/>
+			<meta
+				name='page-type'
+				content='Web-App'
+			/>
+			<meta
+				name='audience'
+				content='Everyone'
+			/>
+			<meta
+				name='og:title'
+				property='og:title'
+				content='Word Count App || MAK Development House || MAKSTYLE119'
+			/>
+			<meta
+				name='robots'
+				content='index, follow'
+			/>
+		</Head>
+	);
+};
